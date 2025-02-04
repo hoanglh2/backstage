8777a48c57e41e508f37f4571d20641fe4e5e31b
hoanglh2 <hoanglh@gearment.com>
2025-02-04 17:40:09 +0700

    init

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -11,6 +11,7 @@ import { createBackend } from '@backstage/backend-defaults';
 const backend = createBackend();
 
 backend.add(import('@backstage/plugin-app-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
